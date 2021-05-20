0491e38b69f4af15e6afefed9f2fd2e8fcfdbcb7
rks-ookuma <takahiro.okuma@rakus.co.jp>
2021-05-20 12:14:12 +0900

    新規にプロジェクト作成（first commit)

---
+++ src/main/java/com/example/GitSampleOokumaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleOokumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleOokumaApplication.class, args);
+	}
+
+}
